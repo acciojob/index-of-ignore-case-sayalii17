2d6b6db8f4f7b92d8c7df67fb53b7a2a3c5c91dd
Sayali Patil <137858267+sayalii17@users.noreply.github.com>
2025-09-10 17:03:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	s1= s1.toLowerCase();
+	s2= s2.toLowerCase();
+
+	if(s1.includes(s2)){
+		return s1.indexOf(s2);
+	}
+
+	return -1;
 }
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
+
+
